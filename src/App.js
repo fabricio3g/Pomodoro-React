d8cf0b3cb008a5ee30cddac4577f8957849a3a3a
fabricio3g <48396639+fabricio3g@users.noreply.github.com>
2020-08-24 13:26:54 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./styles.css";
+import Timer from "./Component/Timer";
+
+export default function App() {
+  return <div className="App">
+    <Timer/>
+  </div>;
+}
